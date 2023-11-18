4e8ef5aa2dd7b439779ab4a91d3e9539bf0e3f54
Alex J Reigle <areigle@infoscitex.com>
2023-11-18 17:04:44 -0500

    Completed Chapter 3 code

--- OneWeekend/OneWeekend.cpp
+++ OneWeekend/OneWeekend.cpp
@@ -2,8 +2,8 @@
 //
 
 #include "OneWeekend.h"
-#include <iostream>
-#include <fstream>
+#include "color.h"
+#include "vec3.h"
 
 int main() {
 
@@ -29,15 +29,8 @@ int main() {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            img << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/ (image_width - 1), double(j) / (image_height - 1), 0);
+            //write_color(std::cout, pixel_color);
         }
     }
 
