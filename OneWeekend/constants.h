182a982362003a6b00138bf0a1d009963d3b7630
Alex J Reigle <areigle@infoscitex.com>
2023-11-20 21:48:44 -0500

    Completed chapter 8 code

--- OneWeekend/constants.h
+++ OneWeekend/constants.h
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+#include <random>
 
 
 // Usings
@@ -26,6 +28,17 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0,1).
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
 // Common Headers
 
 #include "interval.h"
