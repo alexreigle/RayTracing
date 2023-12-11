f4a2e65590b6458c61ae3bf3469d95c861b7f16d
Alex J Reigle <areigle@infoscitex.com>
2023-12-10 20:40:58 -0500

    Completed Chapter 3 Ray Tracing with CUDA code

--- CudaWeekend/src/kernel.cu
+++ CudaWeekend/src/kernel.cu
@@ -8,11 +8,15 @@
 #include <time.h>
 #include <string>
 
+#include "vec3.h"
+#include "ray.h"
+
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 
 void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
-    if (result) {
+    if (result) 
+    {
         std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
             file << ":" << line << " '" << func << "' \n";
         // Make sure we call CUDA Device Reset before exiting
@@ -21,19 +25,31 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
     }
 }
 
-__global__ void render(float* fb, int max_x, int max_y) {
+__device__ vec3 color(const ray& r) 
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5f * (unit_direction.y() + 1.0f);
+    return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+__global__ void render(vec3* fb, int max_x, int max_y,
+                       vec3 lower_left_corner, vec3 horizontal,
+                       vec3 vertical, vec3 origin) 
+{
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if ((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j * max_x * 3 + i * 3;
-    fb[pixel_index + 0] = float(i) / max_x;
-    fb[pixel_index + 1] = float(j) / max_y;
-    fb[pixel_index + 2] = 0.2;
+    int pixel_index = j * max_x + i;
+    float u = float(i) / float(max_x);
+    float v = float(j) / float(max_y);
+    ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+    fb[pixel_index] = color(r);
 }
 
 int main(int argc, char* argv[])
 {
     int nx, ny, tx, ty;
+    /* TODO: Fiddle with tx/ty thread sizes to see what works best*/
 
     if (argc > 1)
     {
@@ -58,7 +74,7 @@ int main(int argc, char* argv[])
     size_t fb_size = 3 * num_pixels * sizeof(float);
 
     // allocate FB
-    float* fb;
+    vec3* fb;
     checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
 
     clock_t start, stop;
@@ -66,7 +82,11 @@ int main(int argc, char* argv[])
     // Render our buffer
     dim3 blocks(nx / tx + 1, ny / ty + 1);
     dim3 threads(tx, ty);
-    render << <blocks, threads >> > (fb, nx, ny);
+    render<<<blocks, threads>>>(fb, nx, ny,
+                                  vec3(-2.0, -1.0, -1.0),
+                                  vec3(4.0, 0.0, 0.0),
+                                  vec3(0.0, 2.0, 0.0),
+                                  vec3(0.0, 0.0, 0.0));
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     stop = clock();
@@ -84,19 +104,16 @@ int main(int argc, char* argv[])
         std::cout << "Error: Invalid File" << std::endl;
         return 1;
     }
-    img << "P3\n" << nx << " " << ny << "\n255\n";
 
+    img << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            size_t pixel_index = j * 3 * nx + i * 3;
-            float r = fb[pixel_index + 0];
-            float g = fb[pixel_index + 1];
-            float b = fb[pixel_index + 2];
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99 * fb[pixel_index].r());
+            int ig = int(255.99 * fb[pixel_index].g());
+            int ib = int(255.99 * fb[pixel_index].b());
             img << ir << " " << ig << " " << ib << "\n";
         }
     }
