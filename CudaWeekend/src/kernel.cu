44ed3a72a1398a33635640b758b75dc719747069
Alex J Reigle <areigle@infoscitex.com>
2023-12-11 19:56:40 -0500

    Complete Chapter 4 Ray Tracing CUDA code

--- CudaWeekend/src/kernel.cu
+++ CudaWeekend/src/kernel.cu
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <time.h>
 #include <string>
+#include <float.h>
 
 #include "vec3.h"
 #include "ray.h"
@@ -25,17 +26,25 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
     }
 }
 
-__device__ vec3 color(const ray& r) 
-{
+__device__ bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4.0f * a * c;
+    return (discriminant > 0.0f);
+}
+
+__device__ vec3 color(const ray& r) {
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+        return vec3(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5f * (unit_direction.y() + 1.0f);
     return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 __global__ void render(vec3* fb, int max_x, int max_y,
-                       vec3 lower_left_corner, vec3 horizontal,
-                       vec3 vertical, vec3 origin) 
-{
+    vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if ((i >= max_x) || (j >= max_y)) return;
@@ -71,7 +80,7 @@ int main(int argc, char* argv[])
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
     int num_pixels = nx * ny;
-    size_t fb_size = 3 * num_pixels * sizeof(float);
+    size_t fb_size = num_pixels * sizeof(vec3);
 
     // allocate FB
     vec3* fb;
@@ -82,11 +91,11 @@ int main(int argc, char* argv[])
     // Render our buffer
     dim3 blocks(nx / tx + 1, ny / ty + 1);
     dim3 threads(tx, ty);
-    render<<<blocks, threads>>>(fb, nx, ny,
-                                  vec3(-2.0, -1.0, -1.0),
-                                  vec3(4.0, 0.0, 0.0),
-                                  vec3(0.0, 2.0, 0.0),
-                                  vec3(0.0, 0.0, 0.0));
+    render << <blocks, threads >> > (fb, nx, ny,
+        vec3(-2.0, -1.0, -1.0),
+        vec3(4.0, 0.0, 0.0),
+        vec3(0.0, 2.0, 0.0),
+        vec3(0.0, 0.0, 0.0));
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     stop = clock();
@@ -106,11 +115,9 @@ int main(int argc, char* argv[])
     }
 
     img << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny - 1; j >= 0; j--)
-    {
-        for (int i = 0; i < nx; i++)
-        {
-            size_t pixel_index = j*nx + i;
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j * nx + i;
             int ir = int(255.99 * fb[pixel_index].r());
             int ig = int(255.99 * fb[pixel_index].g());
             int ib = int(255.99 * fb[pixel_index].b());
